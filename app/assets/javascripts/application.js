e7276728796bb1ecd5b1583bbad7627926de58a9
Amiel Martin <amiel@carnesmedia.com>
2012-09-24 10:11:13 -0700

    Switch to my own customizable tabs,

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
-
+//= require tabs
 
 jQuery(document).ready(function() {
-  $(document).foundationTabs();
+  $(document).tabs();
 });
---
+++ app/assets/javascripts/tabs.js
@@ -0,0 +1,40 @@
+// Amiel's hacking on foundations tabs.
+
+;(function ($, window, undefined) {
+  'use strict';
+
+  $.fn.tabs = function (options) {
+
+    var settings = $.extend({
+      callback: $.noop,
+      suffix: '_tab'
+    }, options);
+
+    var activateTab = function($tab) {
+      var $activeTab = $tab.closest('dl').find('dd.active'),
+          contentLocation = $tab.children('a').attr("href") + settings.suffix;
+
+      // Strip off the current url that IE adds
+      contentLocation = contentLocation.replace(/^.+#/, '#');
+
+      //Make Tab Active
+      $activeTab.removeClass('active');
+      $tab.addClass('active');
+
+      //Show Tab Content
+      $(contentLocation).closest('.tabs-content').children('li').removeClass('active').hide();
+      $(contentLocation).css('display', 'block').addClass('active');
+    };
+
+    $(document).on('click.fndtn', 'dl.tabs dd a', function (event){
+      activateTab($(this).parent('dd'));
+    });
+
+    if (window.location.hash) {
+      activateTab($('a[href="' + window.location.hash + '"]').parent('dd'));
+      settings.callback();
+    }
+
+  };
+
+})(jQuery, this);
