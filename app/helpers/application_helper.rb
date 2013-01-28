7f73716e705fc8a982f1b2234425b195a0418194
Amiel Martin <amiel.martin@gmail.com>
2013-01-27 17:58:08 -0800

    Rearrange so it is all different actions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+
+  def tab(action, title = action.to_s.titleize)
+    html_class = []
+
+
+    if controller.action_name == action.to_s
+      html_class << 'active'
+    end
+
+    content_tag :dd, :class => html_class do
+      link_to title, action: action
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 PassingPedagogy::Application.routes.draw do
+  get 'in_general', to: 'pages#in_general'
+  get 'exercise_1', to: 'pages#exercise_1'
+  get 'exercise_2', to: 'pages#exercise_2'
+  get 'exercise_3', to: 'pages#exercise_3'
+  get 'thanks',     to: 'pages#thanks'
+
   root to: 'pages#index'
 end
